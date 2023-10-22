146f506f15ddd6d3d9372a6785788d517370bda6
Daniil Zverev <svarhik348@gmail.com>
2023-10-22 03:59:58 +0300

    init commit

--- tests/test_moysklad_main_page.py
+++ tests/test_moysklad_main_page.py
@@ -11,7 +11,7 @@ main_page = MainPage()
 @allure.feature('Main page')
 @allure.story('Прооверка авторизации')
 @allure.link('https://www.moysklad.ru/')
-def test_assert_elements_texts():
+def test_open_authorization():
     main_page.open()
 
     main_page.go_to_authorization()
@@ -23,7 +23,7 @@ def test_assert_elements_texts():
 @allure.feature('Main page')
 @allure.story('Прооверка регистрации')
 @allure.link('https://www.moysklad.ru/')
-def test_assert_elements_texts():
+def test_open_registration_form():
     main_page.open()
 
     main_page.registration_form()
@@ -35,7 +35,7 @@ def test_assert_elements_texts():
 @allure.feature('Main page')
 @allure.story('Прооверка тарифа')
 @allure.link('https://www.moysklad.ru/')
-def test_assert_elements_texts():
+def test_check_free_plan():
     main_page.open()
 
     main_page.free_plan()
@@ -46,7 +46,7 @@ def test_assert_elements_texts():
 @allure.feature('Main page')
 @allure.story('Прооверка поиска маркетплейса')
 @allure.link('https://www.moysklad.ru/')
-def test_assert_elements_texts():
+def test_search_marketplace():
     main_page.open()
 
     main_page.search_marketplace()
@@ -58,7 +58,7 @@ def test_assert_elements_texts():
 @allure.feature('Main page')
 @allure.story('Прооверка перехода на youtube канал')
 @allure.link('https://www.moysklad.ru/')
-def test_assert_elements_texts():
+def test_open_youtube_channel():
     main_page.open()
 
     main_page.go_to_youtube_channel()
